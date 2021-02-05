c6c3a4ae209207f39187fea264292e14884ece5f
Thomas Girke <tgirke@citrus.ucr.edu>
2021-02-04 17:43:16 -0800

    some edits

--- renderCommit.sh
+++ renderCommit.sh
@@ -3,14 +3,14 @@
 ## Author: Thomas Girke
 ## Last update: Feb 4, 2021
 
-## (1) Makes sure you are in correct branch
+## (1) Makes sure you are in correct branch (currently there is only one)
 git checkout main  
 
 ## (2) Build site and copy rendered pages to docs 
-rm -rf public/*
-rm -rf docs/*
-Rscript -e "blogdown::build_site()"
-cp -r public/* docs/
+rm -rf public/* # clean out public
+rm -rf docs/* # clean out docs (assumes dir exists)
+Rscript -e "blogdown::build_site()" # Build site with blogdown via command-line call
+cp -r public/* docs/ # Copy build/rendered pages to docs
 
 ## (3) Commit and push changes
 git add -A :/
