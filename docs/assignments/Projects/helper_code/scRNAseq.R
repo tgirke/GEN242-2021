e84249df52326a3f69f82d9ac1871df6a3c5c8e7
Thomas Girke <tgirke@citrus.ucr.edu>
2021-05-30 15:57:37 -0700

    some edits

--- docs/assignments/Projects/helper_code/scRNAseq.R
+++ docs/assignments/Projects/helper_code/scRNAseq.R
@@ -5,10 +5,19 @@
 ## (1) Generate SummarizedExperiment and SingleCellExperiment objects for
 ## RNA-Seq data from Howard et al (2013)
 
-## (1a) Creaat first Summarized Experiment object and then coerce to SingleCellExperiment
+###############################################################
+## Embedding Methods for RNA-Seq and scRNA-Seq Samples/Cells ##
+###############################################################
+
+## (A) Bulk RNA-Seq Data
+
+## (A1) Generate SummarizedExperiment and SingleCellExperiment objects for
+## RNA-Seq data from Howard et al (2013)
+
+## (A1a) Creaat first Summarized Experiment object and then coerce to SingleCellExperiment
 library(SummarizedExperiment); library(SingleCellExperiment)                                                                                                                        
-targetspath <- "targetsPE.txt" # Assumes targetsPE file from course project in your current directory                                                                                                                                                     
-countpath <- "results/countDFeByg.xls" # Assumes full count table from course project in results sub-directory                                                                                                                                            
+targetspath <- "targetsPE.txt"                                                                                                                                                      
+countpath <- "results/countDFeByg.xls"                                                                                                                                              
 targets <- read.delim(targetspath, comment.char = "#")                                                                                                                              
 rownames(targets) <- targets$SampleName                                                                                                                                             
 countDF <- read.delim(countpath, row.names=1, check.names=FALSE)                                                                                                                    
@@ -17,34 +26,73 @@ countDF <- read.delim(countpath, row.names=1, check.names=FALSE)
 ## (1a) The SingleCellExperiment can also be created directly from the two tabular input files like this
 (sce2 <- SingleCellExperiment(assays=list(counts=countDF), colData=targets))
 
-## (2) Prepare data for plotting with run* embedding functions from scran/scater
+## (A2) Prepare data for plotting with run* embedding functions from scran
 library(scran); library(scater)
 sce <- logNormCounts(sce)
 colLabels(sce) <- factor(colData(sce)$Factor) # This uses replicate info from above targets file as pseudo-clusters
 
-## (3) Run and plot with different embedding methods. Note: the embedding
+## (A3) Run and plot with different embedding methods Note: the embedding
 ## results are sequentially appended to the SingleCellExperiment object,
 ## meaning one can use the plot function whenever necessary.
 
-## (3a) Run tSNE on Howard et al data
+## (A3a) Run tSNE on Howard et al data
 sce <- runTSNE(sce)
 reducedDimNames(sce)
 plotTSNE(sce, colour_by="label", text_by="label")
 
-## (3b) Run MDS on Howard et al data
+## (A3b) Run MDS on Howard et al data
 sce <- runMDS(sce)
 reducedDimNames(sce)
 plotMDS(sce, colour_by="label", text_by="label")
 
-## (3c) Run UMAP on Howard et al data
-sce <- runUMAP(sce) # gives a warning but it still works 
+## (A3c) Run UMAP on Howard et al data
+sce <- runUMAP(sce) 
 reducedDimNames(sce)
 plotUMAP(sce, colour_by="label", text_by="label")
 
-## (3d) Run PCA on Howard et al data
-sce <- runPCA(sce) # gives a warning but it still works 
+## (A3d) Run PCA on Howard et al data
+sce <- runPCA(sce) # gives a warning due to small size of data set but it still works 
 reducedDimNames(sce)
 plotPCA(sce, colour_by="label", text_by="label")
 
-## Importantly: now compare the results in the four embedding plots with the sample
-## tree from the RNA-Seq workflow here: https://bit.ly/3c3z1j5
+
+## (B) Single Cell RNA-Seq Data
+
+## (B1) Obtain scRNA-Seq Data from Xenopus tail (Aztekin et al, 2019)
+library(scRNAseq)
+sce <- AztekinTailData()
+
+## (B2) Prepare data for plotting with run* embedding functions from scran
+library(scran); library(scater)
+sce <- logNormCounts(sce)
+clusters <- quickCluster(sce)
+# sce <- computeSumFactors(sce, clusters=clusters)
+colLabels(sce) <- factor(clusters)
+table(colLabels(sce))
+
+## (B3) Run and plot with different embedding methods Note: the embedding
+## results are sequentially appended to the SingleCellExperiment object,
+## meaning one can use the plot function whenever necessary.
+
+## (B3a) Run tSNE on Xenopus scRNA-Seq data
+sce <- runTSNE(sce)
+reducedDimNames(sce)
+plotTSNE(sce, colour_by="label", text_by="label")
+
+## (B3b) Run MDS on Xenopus scRNA-Seq data
+sce <- runMDS(sce)
+reducedDimNames(sce)
+plotMDS(sce, colour_by="label", text_by="label")
+
+## (B3c) Run UMAP on Xenopus scRNA-Seq data
+# sce <- runUMAP(sce) # Note, the UMAP embedding is already stored in downloaded SingleCellExperiment object by authers. So one can just use this one or recompute it. 
+reducedDimNames(sce)
+plotUMAP(sce, colour_by="label", text_by="label")
+
+## (B3d) Run PCA on Xenopus scRNA-Seq data
+sce <- runPCA(sce) 
+reducedDimNames(sce)
+plotPCA(sce, colour_by="label", text_by="label")
+
+
+
